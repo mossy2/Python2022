e8b134c01335ebd087d074d6a658a64fd6a54ce3
mossy2 <amobet31@kingshigh.school.nz>
2022-03-03 09:34:54 +1300

    updating data type

--- 2_data_type.py
+++ 2_data_type.py
@@ -3,12 +3,28 @@ print("with great power comes great responsibility.")
 print("A string is a secuence of characters, visible or unvisible \n it is also always in quotation marks")
 print("an integer is a whole number and does not need quotation marks")
 print("a number that is not an integer")
-print("543/3")
 print(543/3)
+print(int(543/3))
 print(543%3)
-
-
-
+print(1000%9)
+print(434//10)
+print(3%2)
+print(5%5)
+print(555/2)
+print(123/2)
+print(123//2)
+print(123%2)
+print(764%2)
+print(165//2)
+print(2.4+6.7)
+print(2+2)
+print(7-2.4)
+print(2.4-2)
+print(8/4)
+print(2.4*8)
+print(4*7)
+print("amos"*10)
+print("amos\n"*10)
 
 
 
