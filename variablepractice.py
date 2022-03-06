409527ad3a7c97e4bc47fac718a25d764fe9a823
mossy2 <amobet31@kingshigh.school.nz>
2022-03-07 12:22:24 +1300

    variblae practice 3

--- variablepractice.py
+++ variablepractice.py
@@ -9,3 +9,13 @@ name = input(f"enter your name please")
 print(f"{name} is a funny name")
 age = input(f"enter your age please")
 print(f"if your {age} you shouldnt be using this comupter you are underaged")
+
+currentyear=input(f"what is the current year?")
+print(f"the year is {currentyear}")
+
+print(f"your name is {name}")
+print(f"your age is {age}")
+print(f"the year is {currentyear}")
+surname=input(f"enter your surname please")
+school=input(f"what school you at?")
+favfood=input(f"whats your favorite food?")
