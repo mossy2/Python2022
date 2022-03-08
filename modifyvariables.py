8a34beccac3d8678d75527119ab10822d533496a
mossy2 <amobet31@kingshigh.school.nz>
2022-03-08 13:24:43 +1300

    modifyvarible practice

---
+++ modifyvariables.py
@@ -0,0 +1,19 @@
+money=200
+hat=20
+tops=30
+pants=15
+belts=60
+shoes=40
+print("you have 200 dollars")
+print("buying a hat")
+print(money-hat)
+print(f"you now have {money-hat} dollars left")
+money=money-hat
+print(f"money={money}")
+print("buying a top")
+print(money-tops)
+print(f"you now have {money-tops} dollars left")
+money=money-tops
+print(f"money={money}")
+print("buying a pair of pants")
+print(money-pants)
--- variablepractice.py
+++ variablepractice.py
@@ -1,5 +1,5 @@
 playerlives = 100
-print(playerlives)
+print(f"you have {playerlives} lives left")
 username = input("enter a username pls")
 bank = 1000
 print(f"you have {bank} dollars")
@@ -19,3 +19,17 @@ print(f"the year is {currentyear}")
 surname=input(f"enter your surname please")
 school=input(f"what school you at?")
 favfood=input(f"whats your favorite food?")
+petname=input(f"do you have any pets? whats their name?")
+chocblocks=input(f"how many blocks of chocolate do you have?")
+playerscore = 50
+print(f"your score is {playerscore}")
+playerscorehigh = 100
+print(f"your highscore is {playerscorehigh}")
+laps = 13
+print(f"you have ran {laps}")
+ammo = 56
+print(f"you have {ammo} bullets left")
+gear = 4
+print(f"your manualgear is {gear}")
+kmpersecond = 114
+print(f"you are going at {kmpersecond} kilometres per second")
